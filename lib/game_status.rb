43abea0d6b0e540d05a77d9ca195c86be125b173
Austin  Burke <aburk3@gmail.com>
2018-09-13 10:07:25 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -36,3 +36,9 @@ WIN_COMBINATIONS = [
   def over?(board)
     won?(board) || draw?(board) || full?(board)
   end
+  
+  def winner(board)
+    if winning_combo = won?(board)
+      board[winning_combo.first]
+    end
+  end
